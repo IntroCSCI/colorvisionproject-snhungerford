2be85f8acbda0aca70d753df13bfc1dfaa036eb7
snhungerford <72060533+snhungerford@users.noreply.github.com>
2021-03-11 21:58:27 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,53 @@
-//Author: PUT YOUR NAME HERE
+//Author: Shane Hungerford
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+
 using namespace std;
 
-int main()
-{
+int main() {
 
-  return 0;
+int paletteNum;
+int red;
+int blue;
+int green;
+string fileName;
+
+ofstream outfile;
+
+  cout << "How many colors would you like in your palette?\n";
+  cin >> paletteNum;
+  cout << "What is the name of the file you would like your palette saved to?\n";
+  cin >> fileName;
+
+  outfile.open(fileName, ios::out | ios::trunc );
+
+for(int i = 1; i<(paletteNum+1); i++){
+  outfile << "Color #" << i << ": ";
+  
+ outfile << "R";
+  for(int b = 0; b < 3; b++){
+    outfile << rand()%9;
+  }
+  outfile << " ";
+
+  outfile << "G";
+  for(int b = 0; b < 3; b++){
+    outfile << rand()%9;
+  }
+  outfile << " ";
+
+  outfile << "B";
+  for(int b = 0; b < 3; b++){
+    outfile << rand()%9;
+  }
+  outfile << " ";
+  outfile << endl;
+
+}
+outfile << endl;
+outfile.close();
+
+return 0;
 }
